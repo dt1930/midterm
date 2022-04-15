5e5e833c6944ac94c105aa5fd231610dfdb02f51
dt1930 <74736583+dt1930@users.noreply.github.com>
2022-04-15 12:12:05 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-let express=require('express');
+//importing the require modules and creating objects
+let express=require('express'); 
 let http=require('http');
 let app=express();
 let server=http.createServer(app);
 let io= require('socket.io');
-let roomObjects=[];
+
+let roomObjects=[]; // array to hold several rooms connected to the server
 io=new io.Server(server);
 app.use('/',express.static('public'));
+
+//when client tries to connect to the server
 io.sockets.on('connect',(socket)=>{
     console.log(socket.id,"has connected");
     let roomAlreadyExists=false;
     let myRoom;
+    
+    //receving the information about the client 
     socket.on('userData',(data)=>{
         socket.userName=data.userName;
         socket.roomNum=data.room;
         socket.status=true;
+        //checking if the room already exists or not
         for (let i=0; i<roomObjects.length; i++){
             if (socket.roomNum==roomObjects[i].roomNum){
                 roomAlreadyExists=true;
@@ -21,48 +28,56 @@ io.sockets.on('connect',(socket)=>{
                 break;
             }
         }
+        //if the room number doesn't exist
         if (roomAlreadyExists==false){
-            myRoom= new room(socket.roomNum);
-            roomObjects.push(myRoom);
-            socket.join(myRoom.roomNum);
-            myRoom.names.push(socket.userName);
-            socket.emit('userCount',myRoom.value);
+            myRoom= new room(socket.roomNum); //creating a new room object
+            roomObjects.push(myRoom); //inserting the new room into roomObjects array
+            socket.join(myRoom.roomNum); //joining the client to a particular room number
+            myRoom.names.push(socket.userName); //saving the names of the players in names attribute of room
+            socket.emit('userCount',myRoom.value); //increasing the number of players in the room
         }
+        //if the room number already exists
         if (roomAlreadyExists==true){
             if (myRoom.value<2){
                 socket.join(socket.roomNum);
                 myRoom.names.push(socket.userName);
                 myRoom.value+=1;
-                socket.emit('userCount',myRoom.value);
+                socket.emit('userCount',myRoom.value); //emitting the number of players info to the client end
             }
             else{
-                socket.emit('packed',myRoom.value);
+                socket.emit('packed',myRoom.value); //emitting the packed information in case the room is full
                 socket.status=false;
             }
         }
+        // server emitting the information to start the timer
         socket.on('timerStart',(data)=>{
             io.to(socket.roomNum).emit('timerStart',data);
         })
+        //when any socket in the room disconnects
         socket.on('disconnect',()=>{
             if (socket.status==true){
-                myRoom.value--;
+                myRoom.value--; //decrement the number of players in the room
             }
+            //inform the client that the user has disconnected
             io.to(socket.roomNum).emit('userLeft','Your friend has disconnected.Go back to login page!')
             console.log(socket.id, "has disconnected");
             myRoom.fruitsColl.splice(0,myRoom.fruitsColl.length);
             myRoom.capturedFruits=0;
         })
+        //emitting the basket position to the clients after receving the information from the drag user on client side
         socket.on('basketPosition',(data)=>{
             io.to(socket.roomNum).emit('basketPosition',data);
         })
+        //when the fruit is captured/ goes into the basket
         socket.on('capturedFruit',(data)=>{
             for (let i=0; i<myRoom.fruitsColl.length; i++){
                 if (data==myRoom.fruitsColl[i].timestamp){
-                    myRoom.capturedFruits++;
+                    myRoom.capturedFruits++; //score is increased by 1 point
                 }
             }
-            io.to(socket.roomNum).emit('capturedFruit',myRoom.capturedFruits)
+            io.to(socket.roomNum).emit('capturedFruit',myRoom.capturedFruits) //score sent to the clients
         })
+        //to delete the fruit from array when the fruit is clicked and goes off screen
         socket.on('deleteFruit',(data)=>{
             for (let i=0; i<myRoom.fruitsColl.length; i++){
                 if (data==myRoom.fruitsColl[i].timestamp){
@@ -74,14 +89,19 @@ io.sockets.on('connect',(socket)=>{
         socket.on('clickedObj',(data)=>{
             io.to(socket.roomNum).emit('clickedObj',data);
         })
+        //receiving the information about the fruit, saving it in the array, and emitting it back to both clients
+        //this information is received only from the pluck user on the client side
         socket.on('fruitObj',(data)=>{
             myRoom.fruitsColl.push(data);
             myRoom.totalFruits++;
             io.to(socket.roomNum).emit('fruitObj',data);
         })
+        //receiving infromation about the basket drag position (keyboard)
+        //this information is received only from the drag user on the client side
         socket.on('baskDragPos',(data)=>{
             io.to(socket.roomNum).emit('baskDragPos',data);
         })
+        //resetting the game on the server end by re-initializing the room attributes
         socket.on('resetGame',(data)=>{
             if (data){
                 myRoom.capturedFruits=0;
@@ -90,6 +110,7 @@ io.sockets.on('connect',(socket)=>{
             }
             io.to(socket.roomNum).emit('resetGame',true);
         })
+        //sending the score report to the clients when the game ends
         socket.on('gameFinished',(data)=>{
             if (data==true){
                 let scoreObj={
@@ -99,12 +120,11 @@ io.sockets.on('connect',(socket)=>{
                     score:myRoom.capturedFruits
                 }
                 io.to(socket.roomNum).emit('gameFinished',scoreObj);
-                console.log(scoreObj);
             }
         })
     })
 })
-
+//blueprint for the room object
 function room(roomNum){
     this.roomNum=roomNum;
     this.value=1;
@@ -113,7 +133,7 @@ function room(roomNum){
     this.names=[];
     this.totalFruits=0;
 }
-
+//server listening to a certain port
 server.listen(8155,()=>{
     console.log("server up");
 })
